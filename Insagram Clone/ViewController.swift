ee54a4299fbd56d2e672169bdcc7335a19805d4f
denozeobig <86624933+denozeobig@users.noreply.github.com>
2021-12-01 21:48:43 -0500

    Initial Commit

---
+++ Insagram Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Insagram Clone
+//
+//  Created by Daniel Ilin on 01.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
